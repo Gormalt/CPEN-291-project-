a2725d44bb8d51997ec3d7e208ffdc15ecb7289c
Your Name <new2282307660@gmail.com>
2021-03-23 14:21:56 -0700

    testing ml flask integration

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 import io
 import json
 
+import torch
 from torchvision import models
 import torchvision.transforms as transforms
 from PIL import Image
@@ -9,17 +10,12 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 imagenet_class_index = json.load(open('class_index.json')) # class id to name mapping 
-model = models.densenet121(pretrained=True) # define our model here
-model.load_state_dict(torch.load('PATH')) # load the saved model state here
+model = models.resnet18(pretrained=True) # define our model here
+model.load_state_dict(torch.load('birb.pt')) # load the saved model state here
 model.eval()
 
 def transform_image(image_bytes):  # modify the transform function to suit our model
-    my_transforms = transforms.Compose([transforms.Resize(255),
-                                        transforms.CenterCrop(224),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(
-                                            [0.485, 0.456, 0.406],
-                                            [0.229, 0.224, 0.225])])
+    my_transforms = transforms.Compose([transforms.Resize((224,224)), transforms.ToTensor()])
     image = Image.open(io.BytesIO(image_bytes))
     return my_transforms(image).unsqueeze(0)
 
