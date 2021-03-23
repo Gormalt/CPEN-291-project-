6b0fc33de3b87f83c93a8182fb5d435ab98f2486
Your Name <new2282307660@gmail.com>
2021-03-22 21:10:46 -0700

    skeleton code for frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ML for Car Identification</h1>
+      <h3>+</h3>
+    </div>
+  );
+}
+
+export default App;
